aee8ad10f6d2b3e2c84c45af2069cc9a8d0dff3d
unknown <hanifarsyahazis@gmail.com>
2024-06-19 16:58:56 +0700

    Slicing sidebar

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,8 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('layouts.kasir.index');
-})->middleware(['auth'])->name('dashboard');
+Route::middleware(['auth'])->group(function(){
+    Route::get('/dashboard', function() { return view('layouts.kasir.index'); })->name('dashboard');
+    Route::get('/supplier', [PageController::class, 'indexsupplier'])->name('supplier');
+
+});
+
+// Route::get('/dashboard', function () {
+//     return view('layouts.kasir.index');
+// })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
