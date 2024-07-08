5fc1a667089231699dc31760fea5a75f5c9b9016
yazidkurnia <yazidkurniaramadhan@gmail.com>
2024-07-08 09:46:21 +0700

    add crud function

--- app/Http/Controllers/Category/CategoryController.php
+++ app/Http/Controllers/Category/CategoryController.php
@@ -7,5 +7,38 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function indexcategories()
+    {
+        $categories = Categories::select('*')->get();
+
+        return view('layouts.products.categories_data', [
+            'categories' => $categories
+        ]);
+    }
+
+    public function categoriesstore(Request $request)
+    {
+        $categories = new Categories();
+        $categories->nama = $request->nama;
+        $categories->save();
+
+        return response()->json(['success' => TRUE, 'message' => 'Berhasil menyimpan data']);
+    }
+
+    public function categoriesupdate(Request $request, $id)
+    {
+        $categories = Categories::find($id);
+        $categories->nama = $request->nama;
+        $categories->update();
+
+        return response()->json(['success' => TRUE, 'message' => 'Berhasil merubah data']);
+    }
+
+    public function categoriesdestroy(Request $request, $id)
+    {
+        $categories = Categories::findOrFail($id);
+        $categories->delete();
+
+        return response()->json(['success' => TRUE, 'message' => 'Berhasil menghapus data']);
+    }
 }
