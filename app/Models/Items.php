205c3d19ab033d9e7ffa45f9c72f906f3351fb2c
yazidkurnia <yazidkurniaramadhan@gmail.com>
2024-07-26 23:23:05 +0700

    encrypt semua id

--- app/Models/Items.php
+++ app/Models/Items.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Crypt;
 
 class Items extends Model
 {
     protected $table = 'barang';
 
     protected $fillable = ['barcode', 'nama_barang', 'harga_jual', 'harga_modal', 'satuan_id', 'vendor_id', 'kategori_id'];
+
+    public function getIdAttribute($value)
+    {
+        return Crypt::encryptString($value);
+    }
 }
